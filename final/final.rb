dd449ec24e10a5cf989ef6d226eefda1ccd1193c
u-khan <ukhan@ideacouture.com>
2013-06-02 19:07:02 -0300

    updated lamp.rb
    Edited puts questions and removed redundant questions.

--- final/final.rb
+++ final/final.rb
@@ -6,23 +6,22 @@ require_relative "step4.rb"
 require_relative "step5.rb"
 
 def main
+  puts
   puts "Hi!"
   # For lamp.rb
-  puts "Enter password for MySQL servers 'root' user"
+  puts "Enter a password for the MySQL server 'root' user:"
   mysql_root_pass = gets.chomp()
   
   # For step2.rb and step3.rb
-  puts "What is MySQL root password?"
-  db_pass = gets.chomp()
-  puts "What do you want the database to be called?"
+  puts "Enter name of database to be created:"
   new_db_name = gets.chomp()
-  puts "Enter username for new user"
+  puts "Enter a username for new database user:"
   new_db_user = gets.chomp()
-  puts "Enter new user password"
+  puts "Enter a password for new user:"
   newuser_pass = gets.chomp()
   
   # For step4.rb and step5.rb
-  puts "Enter name of site to create"
+  puts "Enter a folder name for the wordpress site:"
   site_name = gets.chomp()
   
   install_lamp(mysql_root_pass)
