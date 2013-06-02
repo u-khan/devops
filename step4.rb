8f4b724599f63072e11c23aa4bbcd2b8d6caa0fc
Umar Khan <umarkhan1@gmail.com>
2013-06-02 13:09:28 -0400

    Added step4.rb

---
+++ step4.rb
@@ -0,0 +1,8 @@
+# Renames wordpress folder and uploads files to '/var/www/'
+# Run with 'rvmsudo ruby step4.rb'
+
+puts "Enter name of blog"
+blog_name = gets.chomp()
+
+system ( "mv ~/wordpress ~/#{blog_name}" )
+system ( "mv ~/blog /var/www/" )
